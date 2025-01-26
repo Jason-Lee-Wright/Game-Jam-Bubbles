e60aa1e787635deeefa53710b7d0f83e5ada644d
Jason <jason974448@gmail.com>
2025-01-26 12:10:52 -0400

    waiting on the [player to input

--- Assets/Scirpts/Timer.cs
+++ Assets/Scirpts/Timer.cs
@@ -11,9 +11,17 @@ public class TimerCountdownScript : MonoBehaviour
 
     void Update()
     {
-        remainingTime -= Time.deltaTime;
-        int minutes = Mathf.FloorToInt(remainingTime / 60);
-        int seconds = Mathf.FloorToInt(remainingTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+            int minutes = Mathf.FloorToInt(remainingTime / 60);
+            int seconds = Mathf.FloorToInt(remainingTime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+
+        if (remainingTime <= 0)
+        {
+            remainingTime = 0;
+        }
     }
 }
